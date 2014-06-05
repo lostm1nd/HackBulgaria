de49c7d86c0b9cdbb6cb42e61074f419564d01f5
lostm1nd <kliomba@abv.bg>
2014-06-05 09:53:31 +0300

    pluck function implemented with map fixes #1

--- FrontEndJS/week1/pluck-func/pluck.js
+++ FrontEndJS/week1/pluck-func/pluck.js
@@ -12,5 +12,14 @@ var pluck = function(property, arr) {
   return result;
 };
 
-exports.pluck = pluck;
+var pluckWithMap = function(property, arr) {
+  // takes and array with objects
+  // and transforms it into a new array
+  // that consist of items which
+  // are the value of the given property
+  return arr.map(function(obj) {
+    return obj[property];
+  });
+};
 
+exports.pluck = pluckWithMap;
