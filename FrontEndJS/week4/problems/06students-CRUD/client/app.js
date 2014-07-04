4270ec49452135c0fa7d41e559a780d6f18e4328
Deyan Yanakiev <kliomba@abv.bg>
2014-07-04 18:05:57 +0300

    implemented adding, retrieving and updating functionality

--- FrontEndJS/week4/problems/06students-CRUD/client/app.js
+++ FrontEndJS/week4/problems/06students-CRUD/client/app.js
@@ -1,15 +1,18 @@
-/* globals $, alert */
+/* globals $, alert, Handlebars */
 
 $(document).ready(function() {
   'use strict';
 
-  var $articleHeaders = $('.row').find('article').find('h2');
+  var $articleHeaders = $('.row').find('article').find('h2'),
+      $studentsContainer = $('#students-container'),
+      studentTemplateSource = $('#student-template').html(),
+      compiledTemplate = Handlebars.compile(studentTemplateSource);
 
   $articleHeaders.on('click', function() {
     $(this).next('.options').slideToggle();
   });
 
-  $('article.add-update-student').find('button').on('click', function() {
+  $('article.add-student').find('button').on('click', function() {
     var $options = $(this).closest('.options'),
         firstName = $options.find('.first-name').val(),
         lastName = $options.find('.last-name').val(),
@@ -31,9 +34,94 @@ $(document).ready(function() {
       data: JSON.stringify(data),
       success: function() {
         alert('Student saved to database.');
+        $options.find('input').val('');
+        $studentsContainer.append(compiledTemplate({students: data}));
       },
       error: function() {
         alert('Something went terribly wrong. Try later.');
+      }
+    });
+  });
+
+  $('article.list-student').on('click', 'h2', function() {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3030/students',
+      dataType: 'json',
+      success: function(students) {
+        var html = compiledTemplate({
+          students: students
+        });
+
+        $studentsContainer.find('header').nextAll().remove();
+        $studentsContainer.append(html).show();
+      }
+    });
+  });
+
+  $('article.delete-student').find('button').on('click', function() {
+    var facultyNumber = $(this).closest('.options').find('input').val();
+    $(this).closest('.options').find('input').val('');
+
+    deleteStudent(facultyNumber);
+  });
+
+  $studentsContainer.on('click', 'button.delete', function() {
+    var facultyNumber = $(this).closest('.row').find('p.faculty-num').text();
+
+    deleteStudent(facultyNumber);
+  });
+
+  function deleteStudent(facultyNumber) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'http://localhost:3030/student/' + facultyNumber,
+      success: function() {
+        $studentsContainer.find('article[data-facultyNum="' + facultyNumber + '"]').remove();
+      },
+      error: function() {
+        alert('Delete request went terribly wrong.');
+      }
+    });
+  }
+
+  $studentsContainer.on('click', 'button.edit', function() {
+    var $parent = $(this).closest('.row'),
+        name = $parent.find('span.name.on-save').hide().text(),
+        courses = $parent.find('span.courses.on-save').hide().text();
+
+    $parent.find('input.name.on-edit').val(name).show();
+    $parent.find('input.courses.on-edit').val(courses).show();
+
+    $(this).hide();
+    $parent.find('button.save').show();
+  });
+
+  $studentsContainer.on('click', 'button.save', function() {
+    var $parent = $(this).closest('.row'),
+        facultyNumber = $parent.find('p.faculty-num').text(),
+        name = $parent.find('input.name.on-edit').hide().val(),
+        courses = $parent.find('input.courses.on-edit').hide().val();
+
+    var updateData = {
+      name: name,
+      facultyNumber: facultyNumber,
+      courses: courses
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3030/student',
+      contentType: 'application/json',
+      data: JSON.stringify(updateData),
+      success: function() {
+        $parent.find('span.name.on-save').show().text(name);
+        $parent.find('span.courses.on-save').show().text(courses);
+        $parent.find('button.save').hide();
+        $parent.find('button.edit').show();
+      },
+      error: function() {
+        alert('Something went terribly wrong when updating the student.');
       }
     });
   });
