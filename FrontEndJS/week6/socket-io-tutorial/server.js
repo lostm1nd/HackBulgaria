72b116981573271492d5d4d8a5d8711c40fc3964
lostm1nd <kliomba@abv.bg>
2014-07-14 15:26:31 +0300

    Trying out socket-io

---
+++ FrontEndJS/week6/socket-io-tutorial/server.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  var app = require('express')(),
+      http = require('http').Server(app),
+      io = require('socket.io')(http);
+
+  app.get('/', function(request, response) {
+    response.sendfile('index.html');
+  });
+
+  io.on('connection', function(socket) {
+    console.log('new user connected');
+
+    socket.on('disconnect', function() {
+      console.log('a user disconnected');
+    });
+
+    socket.on('chat message', function(msg) {
+      console.log(msg);
+      io.emit('chat message', msg);
+    });
+  });
+
+  http.listen(3000, function() {
+    console.log('listening on *:3000');
+  });
+}());
