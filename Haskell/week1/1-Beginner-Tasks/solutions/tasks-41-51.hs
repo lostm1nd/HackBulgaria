9e32586bfba1ce11ffa54b47797cdfb0cd2383b8
Deyan Yanakiev <kliomba@abv.bg>
2015-06-30 21:42:23 +0300

    Final tasks for week1
    Signed-off-by: Deyan Yanakiev <kliomba@abv.bg>

--- Haskell/week1/1-Beginner-Tasks/solutions/tasks-41-51.hs
+++ Haskell/week1/1-Beginner-Tasks/solutions/tasks-41-51.hs
@@ -19,3 +19,19 @@ filterBy p = filter p
 -- task 48
 concat' :: [[a]] -> [a]
 concat' = foldr (\x acc -> foldr (\x acc1 -> x : acc1) acc x) []
+
+-- task 49
+reduce :: (b -> a -> b) -> b -> [a] -> b
+reduce f acc (x:xs) = reduce f (f acc x) xs
+reduce _ acc _ = acc
+
+-- task 50
+reduce' :: (a -> b -> b) -> b -> [a] -> b
+reduce' f acc xs@(x:_) = reduce' f (f (head reversed) acc) (reverse . tail $ reversed)
+	where reversed = reverse xs
+reduce' _ acc _ = acc
+
+-- task 51
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' f (a:as) (b:bs) = f a b : zipWith' f as bs
+zipWith' _ _ _ = []
