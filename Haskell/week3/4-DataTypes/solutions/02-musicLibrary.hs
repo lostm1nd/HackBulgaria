77cf6afd76fe8c78041b192f35a3f29bdbcdcfa6
Deyan Yanakiev <kliomba@abv.bg>
2015-07-10 15:20:20 +0300

    Implement func for manipulating MusicLibrary from week3
    Signed-off-by: Deyan Yanakiev <kliomba@abv.bg>

--- Haskell/week3/4-DataTypes/solutions/02-musicLibrary.hs
+++ Haskell/week3/4-DataTypes/solutions/02-musicLibrary.hs
@@ -1,14 +1,27 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
 type Name = String
 type BirthYear = Int
 type RecordLabel = String
 
 data Author = Author Name BirthYear
-	| Author' Name BirthYear RecordLabel
+			| AuthorInLabel Name BirthYear RecordLabel
+	deriving (Eq, Show)
 
 type Title = String
 type Genre = String
-type LengthInSeconds = Int
+type Length = Double
 
-data Song = Song Title Author Genre LengthInSeconds
+data Song = Song Title Author Genre Length
+	deriving (Eq, Show)
 
-data MusicLibrary = MusicLibrary [Song]
+type MusicLibrary = [Song]
+
+addSong :: MusicLibrary -> Song -> MusicLibrary
+addSong ml s = ml ++ [s]
+
+removeSong :: MusicLibrary -> Song -> MusicLibrary
+removeSong ml s = filter (/= s) ml
+
+removeByAuthor :: MusicLibrary -> Author -> MusicLibrary
+removeByAuthor ml a = filter (\(Song _ a' _ _) -> a /= a' ) ml
