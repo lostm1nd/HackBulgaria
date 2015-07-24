420efbe8b0e5b7e8e7e58823681929593fecc888
Deyan Yanakiev <kliomba@abv.bg>
2015-07-24 21:41:39 +0300

    week6 StatementParser implemented

--- Haskell/week6/1-Applicatives/Parser.hs
+++ Haskell/week6/1-Applicatives/Parser.hs
@@ -1,15 +1,41 @@
 module Parser where
 
-import Control.Applicative
 
-newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
+import Control.Applicative
+import qualified Control.Arrow as A (first)
+-- first (+7) (35, 0) -> (42, 0)
+import qualified Control.Monad as M ((<=<))
+
+
+newtype Parser a =
+  Parser { parse :: String -> Maybe (a, String) }
+
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ fmap (A.first f) . p
+
+instance Applicative Parser where
+  pure a = Parser $ \s -> Just (a, s)
+  Parser fs <*> p = Parser $ \s ->
+    case fs s of
+    Just (f, s') -> parse (fmap f p) s'
+    Nothing      -> Nothing
+-- The same thing, but using `Monad Maybe`
+--Parser fs <*> p = Parser $ uncurry (parse . flip fmap p) M.<=< fs
+
 
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = Parser f
-    where f (x:xs) = if p x then Just (x, xs) else Nothing
+satisfy p = Parser $ \s ->
+  case s of
+  (c:cs) -> if p c then Just (c, cs) else Nothing
+  _      -> Nothing
 
 char :: Char -> Parser Char
-char c = satisfy (\x -> x == c)
+char = satisfy . (==)
+
+string :: String -> Parser String
+string (x:xs) = liftA2 (:) (char x) (string xs)
+string "" = pure ""
 
 openingBrace :: Parser Char
 openingBrace = char '('
@@ -20,25 +46,12 @@ closingBrace = char ')'
 inBraces :: Parser a -> Parser a
 inBraces p = openingBrace *> p <* closingBrace
 
--- fmap :: (a -> b) -> (String -> Maybe (a, String)) -> (String -> Maybe (b, String))
-instance Functor Parser where
-  fmap f (Parser p) = Parser fp
-    where fp xs = fmap (\(a, y) -> (f a, y)) (p xs)
-
--- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-instance Applicative Parser where
-  pure a = Parser (\s -> Just(a, s))
-  Parser f <*> Parser a = Parser pb
-    where pb str = case f str of
-                        Just(a2b, xs) -> fmap (\(a, y) -> (a2b a, y)) (a xs)
-                        Nothing -> Nothing
 
 instance Alternative Parser where
-  empty = Parser (\_ -> Nothing)
-  l <|> r = Parser (\str -> parse l str <|> parse r str)
+  empty = Parser $ const Nothing
+  Parser l <|> Parser r = Parser $ \s -> l s <|> r s
 
-oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = (:) <$> p <*> zeroOrMore p
+oneOrMore, zeroOrMore  :: Parser a -> Parser [a]
+oneOrMore p  = liftA2 (:) p $ zeroOrMore p
 
-zeroOrMore :: Parser a -> Parser [a]
 zeroOrMore p = oneOrMore p <|> pure []
---
+++ Haskell/week6/1-Applicatives/StatementParser.hs
@@ -0,0 +1,92 @@
+module StatementParser where
+
+import Parser
+import Statement
+import Control.Applicative
+import qualified Data.Char as C
+  ( isAlpha
+  , isDigit
+  )
+
+operPlus :: Parser Oper
+operPlus = const Plus <$> char '+'
+
+operMinus :: Parser Oper
+operMinus = const Minus <$> char '-'
+
+operMult :: Parser Oper
+operMult = const Mult <$> char '*'
+
+operDiv :: Parser Oper
+operDiv = const Div <$> char '/'
+
+operMod :: Parser Oper
+operMod = const Mod <$> char '%'
+
+operGt :: Parser Oper
+operGt = const Gt <$> char '>'
+
+operGe :: Parser Oper
+operGe = const Ge <$> string ">="
+
+operLt :: Parser Oper
+operLt = const Lt <$> char '<'
+
+operLe :: Parser Oper
+operLe = const Le <$> string "<="
+
+operEql :: Parser Oper
+operEql = const Eql <$> string "=="
+
+oper :: Parser Oper
+oper = operPlus <|>
+        operMinus <|>
+        operMult <|>
+        operDiv <|>
+        operMod <|>
+        operGt <|>
+        operGe <|>
+        operLt <|>
+        operLe <|>
+        operEql
+
+variable :: Parser Variable
+variable = oneOrMore (satisfy C.isAlpha)
+
+value :: Parser Value
+value =  read <$> (oneOrMore (satisfy C.isDigit))
+
+exprVar :: Parser Expr
+exprVar = Var <$> variable
+
+exprVal :: Parser Expr
+exprVal = Val <$> value
+
+exprOp :: Parser Expr
+exprOp = liftA3 Op expr oper expr
+
+expr :: Parser Expr
+expr = exprVar <|> exprVal <|> exprOp
+
+assignP :: Parser Statement
+assignP = liftA2 Assign (variable <* char '=') expr
+
+incrP :: Parser Statement
+incrP = Incr <$> (string "++" *> variable)
+
+decrP :: Parser Statement
+decrP = Decr <$> (string "--" *> variable)
+
+ifP :: Parser Statement
+ifP = liftA2 If
+    (string "if" *> inBraces expr)
+    (char '{' *> statement <* char '}')
+
+forP :: Parser Statement
+forP = For <$> (string "for" *> char '(' *> statement) <*>
+        (char ';' *> expr) <*>
+        (char ';' *> statement) <*>
+        (char '{' *> statement <* char '}')
+
+statement :: Parser Statement
+statement = assignP <|> incrP <|> decrP <|> ifP <|> forP
